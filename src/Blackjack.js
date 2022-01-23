fea0d57a4f8b3de86193ca66568e6d7b2a5b749e
justinbeaudry10 <jbeaudr5@uwo.ca>
2022-01-23 14:40:14 -0500

    Basic functionality completed

--- src/Blackjack.js
+++ src/Blackjack.js
@@ -1,12 +1,62 @@
 import { useState, useEffect } from "react";
-import Player from "./Player.js";
-import Dealer from "./Dealer.js";
+import Hand from "./Hand.js";
 import "./style.css";
 
 function Blackjack() {
+  const [result, setResult] = useState("");
   const [deck, setDeck] = useState([]);
   const [playerHand, setPlayerHand] = useState([]);
   const [dealerHand, setDealerHand] = useState([]);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [dealerScore, setDealerScore] = useState(0);
+
+  useEffect(() => {
+    let curScore = calcScore(playerHand);
+
+    setPlayerScore(curScore);
+  }, [playerHand]);
+
+  useEffect(() => {
+    if (playerScore === 0) return;
+    checkResult();
+  }, [playerScore]);
+
+  useEffect(() => {
+    if (dealerScore === 0) return;
+    checkResult(true);
+  }, [dealerScore]);
+
+  function calcScore(hand) {
+    let curScore = 0;
+
+    hand.forEach((card) => {
+      if (card.charAt(0) === "A") {
+        curScore += 11;
+      } else if (
+        card.charAt(0) === "J" ||
+        card.charAt(0) === "Q" ||
+        card.charAt(0) === "K" ||
+        card.slice(0, 2) === "10"
+      ) {
+        curScore += 10;
+      } else {
+        curScore += Number(card.charAt(0));
+      }
+    });
+
+    if (curScore > 21) {
+      // First checks if there are any aces, and changes their value from 11 to 1
+      for (let c of hand) {
+        if (c.charAt(0) === "A") {
+          c = "1" + c.slice(1);
+          curScore -= 10;
+          if (curScore <= 21) break;
+        }
+      }
+    }
+
+    return curScore;
+  }
 
   function resetDeck() {
     const suits = ["S", "H", "C", "D"];
@@ -39,6 +89,22 @@ function Blackjack() {
     return newDeck;
   }
 
+  function checkResult(stand = false) {
+    if (playerScore > 21) {
+      setResult("Bust");
+    }
+
+    if (stand) {
+      if (playerScore > dealerScore || dealerScore > 21) {
+        setResult("You Won!");
+      } else if (playerScore === dealerScore) {
+        setResult("Push");
+      } else {
+        setResult("You Lost");
+      }
+    }
+  }
+
   function deal() {
     let curDeck = resetDeck();
 
@@ -53,45 +119,17 @@ function Blackjack() {
       else dealer.push(card);
     }
 
+    setResult("In progress");
     setPlayerHand(player);
     setDealerHand(dealer);
+    setPlayerScore(0);
+    setDealerScore(0);
     setDeck(curDeck);
   }
 
-  function calcScore(hand) {
-    let curScore = 0;
-
-    hand.forEach((card) => {
-      if (card.charAt(0) === "A") {
-        curScore += 11;
-      } else if (
-        card.charAt(0) === "J" ||
-        card.charAt(0) === "Q" ||
-        card.charAt(0) === "K" ||
-        card.slice(0, 2) === "10"
-      ) {
-        curScore += 10;
-      } else {
-        curScore += Number(card.charAt(0));
-      }
-    });
-
-    if (curScore > 21) {
-      for (let c of hand) {
-        if (c.charAt(0) === "A") {
-          c = "1" + c.slice(1);
-          curScore -= 10;
-          break;
-        }
-      }
-    }
-
-    return curScore;
-  }
-
   function hit() {
-    let curDeck = deck;
-    let curPlayerHand = playerHand;
+    let curDeck = [...deck];
+    let curPlayerHand = [...playerHand];
 
     let randIndex = Math.trunc(Math.random() * curDeck.length);
     let card = curDeck[randIndex];
@@ -103,11 +141,58 @@ function Blackjack() {
     setPlayerHand(curPlayerHand);
   }
 
+  function dealerHit() {
+    let curDeck = [...deck];
+    let curDealerHand = [...dealerHand];
+    let curDealerScore = calcScore(curDealerHand);
+
+    while (curDealerScore < 17) {
+      let randIndex = Math.trunc(Math.random() * curDeck.length);
+      let card = curDeck[randIndex];
+      curDeck.splice(randIndex, 1);
+
+      curDealerHand.push(card);
+
+      setDeck(curDeck);
+      setDealerHand(curDealerHand);
+
+      curDealerScore = calcScore(curDealerHand);
+    }
+
+    setDealerScore(curDealerScore);
+    console.log(curDealerScore);
+  }
+
+  function stand() {
+    dealerHit();
+  }
+
   return (
     <>
-      <button onClick={deal}>Deal</button>
-      <button onClick={hit}>Hit</button>
-      <button>Stand</button>
+      <div className="board">
+        <div className="hand">
+          <Hand hand={dealerHand} />
+        </div>
+        <div>
+          {result === "In progress" ? (
+            [
+              <button key="hit" onClick={hit}>
+                Hit
+              </button>,
+              <button key="stand" onClick={stand}>
+                Stand
+              </button>,
+            ]
+          ) : (
+            <button onClick={deal}>Deal</button>
+          )}
+        </div>
+        {result === "In progress" ? null : <label>{result}</label>}
+        <label>Current Score: {playerScore}</label>
+        <div className="hand">
+          <Hand hand={playerHand} />
+        </div>
+      </div>
     </>
   );
 }
