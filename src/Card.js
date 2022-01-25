3c64b47391b926dcfd3c37a5f3a7fa2f596d6c28
justinbeaudry10 <jbeaudr5@uwo.ca>
2022-01-25 16:31:32 -0500

    Responsiveness improvements

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,15 @@
 import React from "react";
+import RenderSmoothImage from "render-smooth-image-react";
+import "render-smooth-image-react/build/style.css";
 
 export default function Card(props) {
-  return <img alt={props.name} src={props.path} className="card" />;
+  return (
+    <div className="cardContainer">
+      <RenderSmoothImage
+        alt={props.name}
+        src={props.path}
+        objectFit="contain"
+      />
+    </div>
+  );
 }
